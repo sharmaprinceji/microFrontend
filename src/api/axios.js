5c7d3c7a8064c3f7797ee4545f8cdf121fbeb3b6
sharmaprinceji <princesh1411@gmail.com>
2026-02-18 15:20:34 +0530

    fix vercel deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -10,8 +10,6 @@ const API = axios.create({
 
 });
 
-
-// REQUEST INTERCEPTOR
 API.interceptors.request.use(
 
   (config) => {
@@ -20,37 +18,13 @@ API.interceptors.request.use(
 
     if (token) {
 
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization =
+        `Bearer ${token}`;
 
-    } else {
-
-      delete config.headers.Authorization;
     }
 
     return config;
-  },
 
-  (error) => Promise.reject(error)
-
-);
-
-
-// RESPONSE INTERCEPTOR (optional but recommended)
-API.interceptors.response.use(
-
-  (response) => response,
-
-  (error) => {
-
-    // auto logout if token invalid
-    if (error.response?.status === 401) {
-
-      localStorage.removeItem("token");
-
-      window.location.href = "/login";
-    }
-
-    return Promise.reject(error);
   }
 
 );
